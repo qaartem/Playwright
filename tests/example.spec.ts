d5e9490df12bfaf2cf4719d5d05fd37103146d60
artem <artem.mindsadyrov@actumdigital.com>
2024-10-18 10:19:56 +0200

    comment test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,25 +1,40 @@
 import { test, expect } from '@playwright/test';
+import { HomePage } from '../pages/homepage';
+import { BlogPage } from '../pages/blogpage';
+import { ArticlePage } from '../pages/articklepage';
+
 
 test('Submit a comment on a blog post and validate the request', async ({ page }) => {
-  await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=common/home');
-  await page.click('.navbar-nav.horizontal .icon-left.both.nav-link .title:has-text("Blog")');
-  await page.locator('.article-thumb.image-top .text-ellipsis-2').first().click();
-  await page.locator('#entry_210911').scrollIntoViewIfNeeded();
-  await page.fill('#input-name', 'example');
-  await page.fill('#input-email', 'example@example.com');
-  await page.fill('#input-comment', 'exampleexampleexampleexampleexampleexampleexampleexample');
+  const name = 'Test';
+  const email = 'test@test.com';
+  const comment = 'TestTestTestTestTestTestTestTestTestTestTest';
+  const successMessage = 'Thank you for your comment. It has been submitted to the webmaster for approval.';
+
   await page.route('**/maza/blog/article/write&article_id=*', (route) => {
     route.continue();
   });
-  await page.click('#button-comment');
-  
-  const [response] = await Promise.all([
-    page.waitForResponse(response => response.status() === 200),
-  ]);
-  const responseBody = await response.json();
-  expect(responseBody).toHaveProperty('success', true);
 
-  await expect(page.locator('.alert-success:has-text(" Thank you for your comment. It has been submitted to the webmaster for approval.")')).toBeVisible();
+  const homePage = new HomePage(page);
+  const blogPage = new BlogPage(page);
+  const articlePage = new ArticlePage(page);
+
+  await homePage.open();
+  await homePage.navigateToBlogSection();
+  await blogPage.clickOnFirstArticle();
+  await articlePage.scrollToCommentForm();
+  const [response] = await Promise.all([
+    page.waitForResponse(response => response.url().includes('/maza/blog/article/write&article_id=') && response.status() === 200),
+    articlePage.fillCommentForm(name, email, comment)
+  ]);
+  
+  const responseBody = await response.json();
+  
+if (responseBody.success === successMessage) {
+  console.log('Success message found:', responseBody.success);
+} else {
+  console.error('Expected success message not found!');
+}
+  await expect(articlePage.successMessage).toHaveText(successMessage);
 });
 
 
