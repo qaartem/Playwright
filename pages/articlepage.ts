a68e7c853cc46c0aa4aa7e1196bd1c22fe110d76
artem <artem.mindsadyrov@actumdigital.com>
2024-10-18 16:48:48 +0200

    add error locators

--- pages/articlepage.ts
+++ pages/articlepage.ts
@@ -8,6 +8,8 @@ export class ArticlePage {
   readonly commentInput: Locator;
   readonly confirmButton: Locator;
   readonly successMessage: Locator;
+  readonly errorNameInputMessage: Locator;
+  readonly errorCommentInputMessage: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -17,6 +19,12 @@ export class ArticlePage {
     this.commentInput = page.locator('#input-comment');
     this.confirmButton = page.locator('#button-comment');
     this.successMessage = page.locator('.alert-success');
+    this.errorNameInputMessage = page.locator('.article-thumb.image-top .text-ellipsis-2');
+    this.errorCommentInputMessage = page.locator('body > div:nth-child(1) > div:nth-child(5) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(9) > form:nth-child(2) > div:nth-child(3) > div:nth-child(2)');
+  }
+
+  async open() {
+    await this.page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=extension/maza/blog/article&article_id=37');
   }
 
   async scrollToCommentForm() {
