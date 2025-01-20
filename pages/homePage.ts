3b6678f85d72efc2a0a4e4dfaf36615dee2d1426
artem <artem.mindsadyrov@actumdigital.com>
2025-01-20 01:18:29 +0100

    fix errors

--- pages/homePage.ts
+++ pages/homePage.ts
@@ -11,7 +11,7 @@ export class HomePage {
   }
 
   async open() {
-    await this.page.goto('https://ecommerce-playground.lambdatest.io/');
+    await this.page.goto('/');
   }
 
   async clickOnBlogButtonOnTheHeaderMenu() {
